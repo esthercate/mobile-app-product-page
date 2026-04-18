59767da1328792d44280d0c5f19d513f2345f5cc
Catherine Vuthi <vuthi.esther@gmail.com>
2026-04-18 22:16:06 +0300

    Remove unused header

--- src/components/sections/partners/Partner.tsx
+++ src/components/sections/partners/Partner.tsx
@@ -3,17 +3,12 @@
 import Image from 'next/image';
 import Container from '@/components/ui/Container';
 import { logos } from './partners.data';
-import SectionHeader from '@/components/ui/SectionHeader';
 
 export function Partner() {
 	return (
 		<section className="py-16">
 			<Container>
 				{/* Heading */}
-				<SectionHeader
-					heading="Trusted by great customers"
-					subheading="We are proud to partner with some of the best companies in the world."
-				/>
 
 				{/* Logos */}
 				<div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6">
